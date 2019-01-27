2e50c1e42b190121bfd7058a82d28876b3c88c65
Miki <miguelsanblassanchez@gmail.com>
2019-01-27 22:31:53 +0100

    Task3: Implementar el comportamiento del boton

--- app/src/main/java/com/example/hellocompat/MainActivity.java
+++ app/src/main/java/com/example/hellocompat/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.hellocompat;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName,"color", getApplicationContext().getPackageName());
+        //int colorRes = getResources().getColor(colorResourceName, this.getTheme());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        mHelloTextView.setTextColor(colorRes);
     }
 }
